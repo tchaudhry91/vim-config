b5c81f20f7173068903bfaca661f1a5cf202cc14
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2017-09-14 16:07:05 +0530

    Added solarized

--- bundle_clones.sh
+++ bundle_clones.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go || true
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree || true
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized || true
